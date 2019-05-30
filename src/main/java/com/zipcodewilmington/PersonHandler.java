b6688825658aa5d25eb3195626faae00f65f1bf2
Edward Korkollie <korkollieed@gmail.com>
2019-05-30 16:25:35 -0400

    Persond Handler lab

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -12,6 +12,15 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
+        int counter = 0;
+        while (counter < personArray.length)
+        {
+
+            Person currentPerson = personArray[counter];
+            String representation = currentPerson.toString();
+            result += representation;
+            counter++;
+        }
         // create a `counter`
         // while `counter` is less than length of array
             // begin loop
@@ -28,6 +37,13 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
+
+        for (int i = 0; i < personArray.length; i++)
+        {
+            Person currentPerson = personArray[i];
+            String representation = currentPerson.toString();
+            result += representation;
+        }
         // identify initial value
         // identify terminal condition
         // identify increment
@@ -46,6 +62,13 @@ public class PersonHandler {
 
     public String forEachLoop() {
         String result = "";
+
+        for(Person currentPerson : personArray)
+        {
+
+            String Representation = currentPerson.toString();
+            result += Representation;
+        }
         // identify array's type
         // identify array's variable-name
 
